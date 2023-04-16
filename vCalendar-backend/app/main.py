a40b59da9b6d48d1eca875bfd6a43965dfb50032
PerfectMach1ne <75753483+PerfectMach1ne@users.noreply.github.com>
2023-04-16 17:56:48 +0200

    Got SQLite & Python project working

---
+++ vCalendar-backend/app/main.py
@@ -0,0 +1,9 @@
+import sqlite3
+import fastapi
+
+from playground.sqlite_test import create_connection
+
+print("please work already")  # yeeeees
+
+if __name__ == "__main__":
+    create_connection(r"D:\Programming\uni-se-prj\vCalendar-backend\database\maindb.db")
