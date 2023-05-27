72363b72f61c2508bae78757f7ba202480f9c9e9
PerfectMach1ne <75753483+PerfectMach1ne@users.noreply.github.com>
2023-05-27 20:23:02 +0200

    refactor: Remove redundant schemas.py code

--- vCalendar-backend/app/schemas.py
+++ vCalendar-backend/app/schemas.py
@@ -1,21 +1,5 @@
 from pydantic import BaseModel
 
-class ItemBase(BaseModel):
-    title: str
-    description: str | None = None
-
-
-class ItemCreate(ItemBase):
-    pass
-
-
-class Item(ItemBase):
-    id: int
-    owner_id: int
-
-    class Config:
-        orm_mode = True
-
 
 class UserBase(BaseModel):
     email: str
@@ -27,8 +11,6 @@ class UserCreate(UserBase):
 
 class User(UserBase):
     id: int
-    is_active: bool
-    items: list[Item] = []
 
     class Config:
         orm_mode = True
