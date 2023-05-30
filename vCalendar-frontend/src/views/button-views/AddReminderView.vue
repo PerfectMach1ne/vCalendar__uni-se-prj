4c5f106b234eb8be1eee93a58c7173807bbe0387
PerfectMach1ne <75753483+PerfectMach1ne@users.noreply.github.com>
2023-05-30 16:24:29 +0200

    feat: Begin implementing AddReminderView page layout

--- vCalendar-frontend/src/views/button-views/AddReminderView.vue
+++ vCalendar-frontend/src/views/button-views/AddReminderView.vue
@@ -11,12 +11,23 @@ export default {
 <template>
   <div class="event__page__container">
     <div class="event__page__box">
-      <p>testtext</p>
-      <button>testbutton</button>
-      <br>
-      <input type="text">
-      <p>testtext</p>
-      <button>testbutton</button>
+      <span>
+        <label for="reminder-title-input">Title: </label>
+        <input id="reminder-title-input" name="reminder-title-input">
+      </span>
+      <span>
+        <label for="reminder-date-input">Date: </label>
+        <input id="reminder-date-input" name="reminder-date-input">
+      </span>
+      <span>
+        <label for="hours-choice">Hours: </label>
+        <input list="hours-list" id="hours-choice" name="hours-choice">
+        <datalist id="hours-list">
+          <option value=""></option>
+          <option v-for="i in 24" v-bind:value="`${String(i - 1).padStart(2, '0') + ':00'}`"></option>
+        </datalist>
+      </span>
+      <button>Create</button>
     </div>
   </div>
 </template>
